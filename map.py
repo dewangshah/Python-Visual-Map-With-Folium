f8f5c6bfb5e6085c4a02d473eb2b7d106f9ebdce
Dewang <dbs20381613@gmail.com>
2020-02-03 17:26:28 -0600

    First Commit

---
+++ map.py
@@ -0,0 +1,36 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+elev=list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation<1000:
+        return 'green'
+    elif elevation>1000 and elevation<3000:
+        return 'orange'
+    else:
+        return 'red'
+
+map=folium.Map(location=[33.588149, -101.880170], zoom_start=7)
+
+fgv=folium.FeatureGroup(name="Volcanoes")
+
+for lt,ln,el in zip(lat,lon,elev):
+    fgv.add_child(folium.CircleMarker(location=[lt,ln],popup="Elevation: "+str(el)+"m", radius=6, fill_color=color_producer(el),color='grey',fill_opacity=0.7))
+
+#fg.add_child(folium.Marker(location=(34.437000, -101.059808),popup="Caprock Canyons State Park", icon=folium.Icon(color='green')))
+#fg.add_child(folium.Marker(location=(34.639042, -100.952220),popup="Palo Duro Canyon", icon=folium.Icon(color='green')))
+
+fgp=folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(),style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']<10000000
+else 'orange' if 10000000 <= x['properties']['POP2005'] <20000000 else 'red'}))
+
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("myMap.html")
